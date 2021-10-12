698fbd2c17bced905ef4b4dddd0b4af4d92673a4
Nir Shihor <nirshihor@gmail.com>
2021-10-12 12:44:22 +0100

    e

--- app.js
+++ app.js
@@ -82,6 +82,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use((req, res, next) => {
     if (req.header('x-forwarded-proto') !== 'https') {
       res.redirect(`https://${req.header('host')}${req.url}`);
+      app.use('/', require('./routes/index'));
+      app.use('/users', require('./routes/users'));
     } else {
       next();
     }
